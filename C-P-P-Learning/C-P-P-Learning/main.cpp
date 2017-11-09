a79593acf3fd56636425cd8c082c6b002d7d80ca
大祭 <lidong.cld@alibaba-inc.com>
2017-11-09 12:07:24 +0800

    [new Project]

---
+++ C-P-P-Learning/C-P-P-Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C-P-P-Learning
+//
+//  Created by Lee on 2017/11/9.
+//  Copyright © 2017年 YYCoding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
